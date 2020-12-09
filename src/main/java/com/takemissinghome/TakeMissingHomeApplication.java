d6ab8a6652af8824868e851877e7712d4a0a26a3
BangKiHyun <rlrlvh@naver.com>
2020-12-09 16:24:26 +0900

    init

---
+++ src/main/java/com/takemissinghome/TakeMissingHomeApplication.java
@@ -0,0 +1,13 @@
+package com.takemissinghome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakeMissingHomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TakeMissingHomeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/takemissinghome/TakeMissingHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.takemissinghome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakeMissingHomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
