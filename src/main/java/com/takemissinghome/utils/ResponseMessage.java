a682a360f22a2f302788c721c57435665cb674b4
BangKiHyun <rlrlvh@naver.com>
2021-01-21 22:40:08 +0900

    refactor: change response entity

---
+++ src/main/java/com/takemissinghome/utils/ResponseMessage.java
@@ -0,0 +1,8 @@
+package com.takemissinghome.utils;
+
+public class ResponseMessage {
+
+    //weak person
+    public static final String RENEW_WEAK_PERSON = "사회적 약자 혜택 정보 갱신 성공";
+    public static final String RENEW_WEAK_PERSON_FAIL = "사회적 약자 혜택 정보 갱신 실패";
+}
--- src/main/java/com/takemissinghome/weakperson/api/WeakPersonController.java
+++ src/main/java/com/takemissinghome/weakperson/api/WeakPersonController.java
@@ -3,12 +3,12 @@ package com.takemissinghome.weakperson.api;
 import com.takemissinghome.gov.response.Content;
 import com.takemissinghome.gov.response.ResponseModel;
 import com.takemissinghome.gov.service.OpenApiService;
+import com.takemissinghome.utils.DefaultResponse;
 import com.takemissinghome.weakperson.api.dto.request.BenefitDataRequest;
 import com.takemissinghome.weakperson.api.dto.request.RenewRequest;
 import com.takemissinghome.weakperson.api.dto.request.WeakPersonDetailRequest;
 import com.takemissinghome.weakperson.service.WeakPersonService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+import static com.takemissinghome.utils.DefaultResponse.res;
+import static com.takemissinghome.utils.ResponseMessage.*;
+import static com.takemissinghome.utils.StatusCode.*;
 import static java.util.stream.Collectors.*;
 
 @RestController
@@ -27,16 +30,16 @@ public class WeakPersonController {
     private final OpenApiService openApiService;
 
     @PostMapping("/renew")
-    public ResponseEntity<String> renewWeakPersonData(@RequestBody RenewRequest renewRequest) {
+    public DefaultResponse<Void> renewWeakPersonData(@RequestBody RenewRequest renewRequest) {
         try {
             String benefitCode = renewRequest.getBenefitCode();
             String weakPersonCode = renewRequest.getWeakPersonCode();
             final ResponseModel responseModel = openApiService.getBenefitDataOfWeakPerson(benefitCode, weakPersonCode);
 
             weakPersonService.renewData(new WeakPersonDetailRequest(benefitCode, weakPersonCode, toBenefitDataList(responseModel)));
-            return ResponseEntity.ok().body("success");
+            return res(OK, RENEW_WEAK_PERSON);
         } catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
+            return res(BAD_REQUEST, RENEW_WEAK_PERSON_FAIL);
         }
     }
 
