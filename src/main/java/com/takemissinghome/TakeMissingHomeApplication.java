7ae2dc77695f6428522589192242351b0245b10b
BangKiHyun <rlrlvh@naver.com>
2021-02-01 14:59:03 +0900

    refactor: move queryFactory injection

--- src/main/java/com/takemissinghome/TakeMissingHomeApplication.java
+++ src/main/java/com/takemissinghome/TakeMissingHomeApplication.java
@@ -1,23 +1,12 @@
 package com.takemissinghome;
 
-import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import javax.persistence.EntityManager;
-
-@EnableJpaAuditing
 @SpringBootApplication
 public class TakeMissingHomeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TakeMissingHomeApplication.class, args);
-    }
-
-    @Bean
-    JPAQueryFactory jpaQueryFactory(EntityManager em) {
-        return new JPAQueryFactory(em);
     }
 }
