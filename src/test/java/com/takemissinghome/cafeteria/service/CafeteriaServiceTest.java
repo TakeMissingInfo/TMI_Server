9cd445adec9dc4999c4ca99cb2a346afec68eaba
BangKiHyun <rlrlvh@naver.com>
2021-02-06 11:59:41 +0900

    test: add cafeteria service renew test

---
+++ src/test/java/com/takemissinghome/cafeteria/service/CafeteriaServiceTest.java
@@ -0,0 +1,37 @@
+package com.takemissinghome.cafeteria.service;
+
+import com.takemissinghome.cafeteria.api.request.CafeteriaRenewRequest;
+import com.takemissinghome.cafeteria.api.response.CafeteriaOpenApiResponse.Item;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+public class CafeteriaServiceTest {
+
+    @Autowired
+    private CafeteriaService cafeteriaService;
+
+    @DisplayName("급식소 데이터를 갱신한다.")
+    @Test
+    public void renewTest() throws Exception {
+        //given
+        final CafeteriaRenewRequest cafeteriaRenewRequest = new CafeteriaRenewRequest(new Item());
+        final List<CafeteriaRenewRequest> cafeteriaRenewRequests = asList(cafeteriaRenewRequest);
+
+        //when
+        final Integer renewSize = cafeteriaService.renew(cafeteriaRenewRequests);
+
+        //then
+        assertThat(renewSize).isEqualTo(cafeteriaRenewRequests.size());
+    }
+}
